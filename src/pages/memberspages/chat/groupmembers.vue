b0cedbcfe74c32be818fadf4d2d7381d796c5f19
liuwejie <1835225627@qq.com>
2020-04-04 19:23:24 +0800

    社团新闻列表+最近活动列表

---
+++ src/pages/memberspages/chat/groupmembers.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='groupmembers'></div>
+</template>
+
+<script>
+export default {
+  name: 'groupmembers',
+  data () {
+    return {}
+  },
+  components: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style>
+</style>
---
+++ src/pages/memberspages/info/info.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='info'></div>
+</template>
+
+<script>
+export default {
+  name: 'info',
+  data () {
+    return {}
+  },
+  components: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style>
+</style>
